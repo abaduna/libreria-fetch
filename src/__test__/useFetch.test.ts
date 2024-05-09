c60d823bf6e11767b8f0e29a57093baa93263ce0
abaduna~ <artuelrey@gmail.com>
2024-05-09 11:45:24 -0300

    test: parcial useAxios

--- src/__test__/useFetch.test.ts
+++ src/__test__/useFetch.test.ts
@@ -9,11 +9,28 @@ import { useAxios } from "../hooks/useFetch"
 
 describe("useAxios",()=>{
     it("should fetch in useEfect",()=>{
-        const {result}=renderHook(()=>useAxios({method:"get",url:"https://rickandmortyapi.com/api/character",enabled:true})) 
+        const {result}=renderHook(()=>useAxios({method:"get",url:"https://rickandmortyapi.com/api/character",enabled:false})) 
+        expect(result.current.loading).toBe(false)
+        act(()=>{
+            result.current.fetcher
+        })
+        expect(result.current.loading).toBe(false)
+    })
+    it("should fetch in useEfect",()=>{
+        const {result}=renderHook(()=>useAxios({method:"get",url:"https://rickandmortyapi.com/api/character",enabled:false})) 
         expect(result.current.data).toBe("")
         act(()=>{
             result.current.fetcher
         })
-        expect(result.current.loading).toBe(true)
+        expect(result.current.data).toBe("")
+    })
+    it("should fetch in useEfect",()=>{
+        const {result,rerender}=renderHook(()=>useAxios({method:"get",url:"https://rickandmortyapi.com/api/character",enabled:true})) 
+        expect(result.current.data).toBe("")
+        act(()=>{
+            result.current.fetcher
+        })
+        rerender() 
+        expect(result.current.data).toBe("")
     })
 })
